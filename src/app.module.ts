3137484682016191dc5a17c4abd84db9314272ed
Hazem Safwat <HSafwat@moj.gov.sa>
2024-07-08 17:25:31 +0300

    "Add User schema and update package dependencies"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,42 @@
-import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
-import {AppController} from './controllers/app.controller';
-import {AppService} from './services/app.service';
-import {MongodbModule} from './config/mongodb.module';
-import {HttpModule} from "@nestjs/axios";
-import {AuthApiService} from "./api-services/auth-api/auth-api.service";
-import {CheckHeaderMiddleware} from "./core/platform-key-middleware/check-header.middleware";
-import {JwtStrategy} from "./core/jwt-auth-guard/jwt.strategy";
-
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { MongodbModule } from './config/mongodb.module';
+import { HttpModule } from '@nestjs/axios';
+import { CheckHeaderMiddleware } from './core/platform-key-middleware/check-header.middleware';
+import { JwtStrategy } from './core/jwt-auth-guard/jwt.strategy';
+import { WpIntegrationController } from './models/wp-integration/wp-integration.controller';
+import { WpIntegrationService } from './models/wp-integration/wp-integration.service';
+import { AuthService } from './models/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-    imports: [
-        MongodbModule,
-        HttpModule,
-    ],
-    controllers: [AppController],
-    providers: [
-        AppService,
-        AuthApiService,
-        JwtStrategy
-    ],
+  imports: [MongodbModule, HttpModule, UsersModule],
+  controllers: [AppController, WpIntegrationController],
+  providers: [
+    AppService,
+    AuthService,
+    JwtStrategy,
+    WpIntegrationService,
+    JwtServic,
+  , // Add UsersService here
 })
 export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(CheckHeaderMiddleware /* , otherMiddleWare */)
-            .forRoutes({path: '*', method: RequestMethod.ALL} /* OR AppController */);
-        /*  // to implement other middleware:
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CheckHeaderMiddleware /* , otherMiddleWare */)
+      .forRoutes(
+        { path: "*", method: RequestMethod.ALL } /* OR AppController */
+      );
+    /*  // to implement other middleware:
          consumer
               .apply(NewMiddleware)
               .forRoutes({ path: 'demo', method: RequestMethod.GET });*/
-
-    }
-
+  }
 }
--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,29 +1,81 @@
-import {Controller, Get, UseGuards,} from '@nestjs/common';
-import {AppService} from '../services/app.service';
-import {ApiBearerAuth, ApiOperation, ApiResponse} from "@nestjs/swagger";
-import {JwtAuthGuard} from "../core/jwt-auth-guard/jwt-auth.guard";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AppService } from '../services/app.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../core/jwt-auth-guard/jwt-auth.guard';
+import { AuthService } from '../models/auth/auth.service';
+import { UsersService } from '../models/users/users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+    private readonly usersService: UsersServic,
+  ) {}
+
+  @Get()
+  @ApiBearerAuth('ac"access-token" // @ApiOperation({summary: 'Check if the API is working'})
+  // @ApiResponse({status: 200, description: 'API is working correctly.'})
+  isWorking(): string {
+    return this.appService.isWorking();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("demo")
+  @ApiBearerAuth("access-token")
+  @ApiOperation({ summary: "Demo route" })
+  @ApiResponse({ status: 200, description: "Returns a demo text." })
+  demo(): string {
+    return "demo";
+  }
+
+  /**
+   * Logs in a user.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} req.user - The user object.
+   * @returns {Promise} - A promise resolving to the result of the login operation.
+   */
+  @UseGuards(AuthGuard("local"))
+  @Post("login")
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Post("register")
+  async createUser(@Body() user: any) {
+    console.log("user", user);
+    const { username, password, email } = user;
+    return await this.usersService.create({ username, password, email });
+  }
+
+  @Get("auth/google")
+  @UseGuards(AuthGuard("google"))
+  googleLogin() {
+    // initiates the Google OAuth2 login flow
+  }
+
+  @Get("auth/google/callback")
+  @UseGuards(AuthGuard("google"))
+  googleLoginCallback(@Req() req: any, @Res() res: any) {
+    console.log("user", req.user);
+    // handles the Google OAuth2 callback
+    const jwt: string = req.user?.accessToken;
+    if (jwt) {
+      this.authService.login({ username: req.user.email });
     }
 
-
-    @Get()
-    @ApiBearerAuth('access-token')
-    // @ApiOperation({summary: 'Check if the API is working'})
-    // @ApiResponse({status: 200, description: 'API is working correctly.'})
-    isWorking(): string {
-        return this.appService.isWorking();
-    }
-
-
-    @UseGuards(JwtAuthGuard)
-    @Get('demo')
-    @ApiBearerAuth('access-token')
-    @ApiOperation({summary: 'Demo route'})
-    @ApiResponse({status: 200, description: 'Returns a demo text.'})
-    demo(): string {
-        return 'demo';
-    }
+    // res.redirect('http://localhost:3000/login/success/' + jwt);
+    // else res.redirect('http://localhost:3000/login/failure');
+  }
 }
